0ac8a6be9c664d0b6b4dfa30ea2e5c71abd40581
ANMOL AGARWAL <24794539+fineanmol@users.noreply.github.com>
2020-08-26 01:57:26 +0530

    notification, advertisement layout created

--- src/containers/MainContent/Advertisements/approved_advertisements.js
+++ src/containers/MainContent/Advertisements/approved_advertisements.js
@@ -8,9 +8,65 @@ import firebase from '.../../../src/firebase';
 
 class approved_advertisements extends Component{
   render(){
-    return(
-      <div></div>
-    );
-  }
-};
+        return(
+            <AUX>
+                <div className="row">
+                            <div className="col-sm-12">
+                                <div className="page-title-box">
+                                <h4 class="page-title">Approved Advertisements</h4>
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="javascript:void(0);">Mirai Vizon</a></li>
+                                        <li class="breadcrumb-item active">Approved Adv</li>
+                                    </ol>
+                <div class="row">
+                            
+                            <div class="col-lg-4">
+                                <div class="card m-b-20">
+                                    <div class="card-body">
+        
+                                        <h4 class="mt-0 header-title">Screen Location Name   |   Screen No <span class="badge badge-danger">3</span></h4>
+                                        <p class="text-muted m-b-30">
+                                            <ul class="mt-0 header-title">
+                                                <li>Company Name <span class="badge badge-primary float-right">Amazon Corp</span></li>
+                                        </ul>
+                                                  
+                                        </p>
+
+                                        <a class="image-popup-no-margins" href="assets/images/small/img-5.jpg">
+                                            <img className="card-img-top img-fluid m-b-30" src="assets/images/small/img-5.jpg" alt="Card image cap"/></a>
+        
+                                        <p>
+                                          
+                                            <button class="btn btn-outline-primary mo-mb-2 waves-effect waves-light " type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                                More Details
+                                            </button>
+                                            
+                                            
+                                        </p>
+                                        <div class="collapse" id="collapseExample">
+                                            <div class="card card-body">
+                                                <ul class="mt-0 header-title">
+                                                    <li>City <span class="badge badge-danger float-right">Noida</span></li>
+                                                    <li>Start Date <span class="badge badge-success float-right">20-12-2020</span> End Date <span class="badge badge-info float-right">22-12-2020</span> </li>
+                                                     <li>Start Time <span class="badge badge-success float-right">5:00</span>  End Time <span class="badge badge-info float-right">7:00</span> </li>
+                                                    <li >Total Amount Paid <span class="badge badge-primary float-right">$50</span></li>
+                                                    <li>Age Range: <span class="badge badge-warning float-right">18-24</span></li>
+                                                    <li>Time-wise Distribution <span class="badge badge-danger float-right">9AM-11AM</span></li>
+                                                    <li>Gender Ratio <span class="badge badge-dark float-right"> 57%</span> </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+        
+                                    </div>
+                                </div>
+                            </div>
+                                    
+                                </div>
+                            </div>
+                        </div></div>
+                        
+            </AUX>
+        );
+    }
+}
 export default approved_advertisements;
--- src/containers/MainContent/Notification/Notification.js
+++ src/containers/MainContent/Notification/Notification.js
@@ -7,10 +7,67 @@ import { Link } from 'react-router-dom';
 import firebase from '.../../../src/firebase';
 
 class Notification extends Component{
-  render(){
-    return(
-      <div></div>
-    );
-  }
-};
+ render(){
+        return(
+            <AUX>
+                <div className="row">
+                            <div className="col-sm-12">
+                                <div className="page-title-box">
+                                <h4 className="page-title">Add Notification</h4>
+                                    
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        
+                         <div class="card m-b-20">
+                                    <div class="card-body">
+        
+                                        <h4 class="mt-0 header-title">Mirai Vizion Application</h4>
+                                        <p class="text-muted m-b-30 ">Mobile App Notifications allow you to keep in touch with your users in a non-intrusive way by providing timely messages and helpful and relevant information like rewards descriptions and special offers. They also provide a great way of getting users to engage in new promotions or features..</p>
+        
+                                        <form class="" action="sendMessage()">
+                                            <div class="form-group">
+                                                <label>Title</label>
+                                                <input type="text" class="form-control" required placeholder="Type something"/>
+                                            </div>
+        
+                                          
+                                          
+                                            <div class="form-group">
+                                                <label>Message</label>
+                                                <div>
+                                                    <textarea required class="form-control" rows="5"></textarea>
+                                                </div>
+                                            </div>
+                                              
+                                        
+                                            <div class="form-group">
+                                                <label>URL (Optional)</label>
+                                                <div>
+                                                    <input parsley-type="url" type="url" class="form-control"
+                                                             placeholder="URL"/>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div>
+                                                    <button type="submit" class="btn btn-primary waves-effect waves-light">
+                                                        Submit
+                                                    </button>
+                                                    <button type="reset" class="btn btn-secondary waves-effect m-l-5">
+                                                        Cancel
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+        
+                                    </div>
+                                </div>
+                            
+        
+        
+            </AUX>
+        );
+    }
+}
 export default Notification;
