89060b1e2bb4d6e50e804fb9286926e725de4c3c
Nikhil Nishad <nikhilnishad279@gmail.com>
2020-08-27 23:51:17 +0530

    add_screen functionality added.

--- src/containers/MainContent/Screens/add_screen.js
+++ src/containers/MainContent/Screens/add_screen.js
@@ -2,20 +2,41 @@ import React, { Component } from 'react';
 import AUX from '../../../hoc/Aux_';
 import Tinycharts from '../../Chartstypes/Tinycharts';
 import { Link } from 'react-router-dom';
+import firebase from '.../../../src/firebase';
 
 class add_screen extends Component {
 
   constructor(props) {
     super(props);
+    this.screensDataRef = firebase.database().ref('AvailableLocations');
     this.state = {
-      switch3: true
+      switch: false,
     };
-
-    //CSS Switch 
-    this.changeswitch3 = this.changeswitch3.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.changeswitch = this.changeswitch.bind(this);
   }
-  changeswitch3() { this.setState({ switch3: !this.state.switch3 }); }
+  changeswitch() { this.setState({ switch: !this.state.switch }); }
 
+  onSubmit(e) {
+    e.preventDefault();
+    let localScreenData = {}
+    if (this.state.switch == false) {
+      localScreenData.screenStatus = "0";
+    }
+    else {
+      localScreenData.screenStatus = "1";
+    }
+    localScreenData.screenId="SCRN02XBF"
+    localScreenData.screenCity = this.screenCity.value;
+    localScreenData.screenImpressions = this.impression.value;
+    localScreenData.screenPrice = this.price.value;
+    localScreenData.screenGenderRatio = this.gender_ratio.value;
+    this.screensDataRef.child(localScreenData.screenCity + "/screenData/" + localScreenData.screenId)
+      .set(localScreenData)
+      .then(_ => {
+        alert("Screen Added.")
+      });
+  }
   render() {
     return (
       <AUX>
@@ -43,7 +64,7 @@ class add_screen extends Component {
 
                 <p className="text-muted m-b-30 font-14">Mobile App Notifications allow you to keep in touch with your users in a non-intrusive way by providing timely messages and helpful and relevant information like rewards descriptions and special offers. They also provide a great way of getting users to engage in new promotions or features...</p>
                 <div className="row">
-                  <div className="col-4">
+                  <div className="col-6">
                     <div className="form-group">
                       <label className="control-label">Select Location</label>
                       <select className="form-control select2">
@@ -54,58 +75,58 @@ class add_screen extends Component {
                         </optgroup>
                       </select></div>
                   </div>
-                  <div className="col-4">
+                  <div className="col-6">
                     <label>Screen Status</label><br></br>
-                    <input type="checkbox" id="switch3" switch="bool" checked={this.state.switch3 ? "checked" : ""} onChange={this.changeswitch3} />
-                    <label for="switch3" data-on-label="On" data-off-label="Off"></label>
+                    <input type="checkbox" id="screenStatus" switch="bool" checked={this.state.switch ? "checked" : ""} onChange={this.changeswitch} />
+                    <label for="screenStatus" data-on-label="On" data-off-label="Off"></label>
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-4">
+                  <div className="col-6">
                     <div className="form-group">
-                      <label for="screen-city" className="col-form-label">City</label>
-                      <input className="form-control" type="text" id="screen-city" />
+                      <label className="col-form-label">City</label>
+                      <input className="form-control" defaultValue="" type="text" name='screenCity' ref={(c) => this.screenCity = c} />
                     </div>
 
                   </div>
-                  <div className="col-4">
+                  <div className="col-6">
                     <div className="form-group">
-                      <label for="screen-city-pincode" className="col-form-label">Pin Code</label>
-                      <input className="form-control" type="text" id="screen-city-pincode" />
+                      <label className="col-form-label">Pin Code</label>
+                      <input className="form-control" defaultValue="" type="text" name="pin_code" ref={(c) => this.pin_code = c} />
                     </div>
 
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-4">
+                  <div className="col-6">
                     <div className="form-group">
-                      <label for="screen-impressions" className="col-form-label">Impressions Per Day</label>
-                      <input className="form-control" type="number" value="42" id="screen-impressions" />
+                      <label className="col-form-label">Impressions Per Day</label>
+                      <input className="form-control" type="number" defaultValue="" name="impression" ref={(c) => this.impression = c} />
                     </div>
                   </div>
-                  <div className="col-4">
+                  <div className="col-6">
                     <div className="form-group">
-                      <label for="screen-impressions" className="col-form-label">Gender Ratio</label>
-                      <input className="form-control" type="number" value="42" id="screen-impressions" />
+                      <label className="col-form-label">Gender Ratio</label>
+                      <input className="form-control" type="number" defaultValue="" name="gender_ratio" ref={(c) => this.gender_ratio = c} />
                     </div>
                   </div>
                 </div>
                 <div className="row">
                   {/* <div className="col-4">
+                            <div className="form-group">
+                            <label for="screen-impressions" className="col-form-label">Time Wise Distribution</label>
+                            <input className="form-control" type="number" value="42" id="screen-impressions" />
+                            </div>
+                        </div> */}
+                  <div className="col-12">
                     <div className="form-group">
-                      <label for="screen-impressions" className="col-form-label">Time Wise Distribution</label>
-                      <input className="form-control" type="number" value="42" id="screen-impressions" />
-                    </div>
-                  </div> */}
-                  <div className="col-8">
-                    <div className="form-group">
-                      <label for="screen-impressions" className="col-form-label">Price</label>
-                      <input className="form-control" type="number" value="42" id="screen-impressions" />
+                      <label className="col-form-label">Price</label>
+                      <input className="form-control" type="number" defaultValue="" name="price" ref={(c) => this.price = c} />
                     </div>
                   </div>
                 </div>
                 <div className="form-group button-items">
-                  <button className="btn btn-primary waves-effect waves-light" type="submit">Submit</button>
+                  <button className="btn btn-primary waves-effect waves-light" type="submit" onClick={this.onSubmit}>Submit</button>
                   <button className="btn btn-secondary waves-effect waves-light" type="submit">Cancel</button>
                 </div>
               </div>
