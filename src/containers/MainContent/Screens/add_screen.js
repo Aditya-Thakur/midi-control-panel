161083dca2c4d54fcd2323828a92dfa57526998a
ANMOL AGARWAL <24794539+fineanmol@users.noreply.github.com>
2020-09-06 02:09:54 +0530

    screen add validation added

--- src/containers/MainContent/Screens/add_screen.js
+++ src/containers/MainContent/Screens/add_screen.js
@@ -8,22 +8,27 @@ import SearchLocationInput from '../Maps/SearchLocationInput'
 
 class add_screen extends Component {
 
+
+
   constructor(props) {
     super(props);
     this.screensDataRef = firebase.database().ref('AvailableLocations');
     this.state = {
-      switch: false,
+      switch: true,
       ageGroupPref:{}
     };
     this.onSubmit = this.onSubmit.bind(this);
     this.changeswitch = this.changeswitch.bind(this);
     this.handleAgeGroupChange = this.handleAgeGroupChange.bind(this);
+	this.dialogAlert = this.dialogAlert.bind(this)
+
   }
   changeswitch() { this.setState({ switch: !this.state.switch }); }
 
   onSubmit(e) {
     e.preventDefault();
     let localScreenData = {}
+
     if (this.state.switch == false) {
       localScreenData.screenStatus = "0";
     }
@@ -31,7 +36,29 @@ class add_screen extends Component {
       localScreenData.screenStatus = "1";
     }
     localScreenData.screenId="SCRN02XBF"
-    localScreenData.screenCity = this.screenCity.value;
+      
+	if(this.screenCity.value=="Select"){
+	   this.dialogAlert("City Required")
+	}
+    if(this.screenLocation.value==""){
+          this.dialogAlert("Screen Location Required")}
+
+    if(this.impression.value==""){
+            this.dialogAlert("Impression value Required")}
+    if(this.price.value==""){
+          this.dialogAlert("Price Required")}
+    if(this.screenPincode.value==""){
+          this.dialogAlert("Screen PinCode Required")}
+    if(this.gender_ratio.value==""){
+          this.dialogAlert("Gender Ratio Required")}
+    if(this.screenLocation.value==""){
+          this.dialogAlert("Screen Location Required")}
+      if(this.state.ageGroupPref.value==""){
+          this.dialogAlert("Age Pref Required")}
+    if(this.screenActiveTime.value==""){
+          this.dialogAlert("Screen Active Time Required")}
+      else{
+          localScreenData.screenCity = this.screenCity.value;
     localScreenData.screenLocation = this.screenLocation.value;
     localScreenData.screenImpressions = this.impression.value;
     localScreenData.screenPrice = this.price.value;
@@ -45,12 +72,25 @@ class add_screen extends Component {
         alert("Screen Added.")
       });
     console.warn(this.state)
+          
+          
+      }
+      
+      
+    
   }
 
   handleAgeGroupChange(event) {
     const target = event.target;
     this.state.ageGroupPref[target.name] = target.value;   
 }
+
+
+dialogAlert(e){
+
+alert(e)
+}
+
   render() {
     return (
       <AUX>
@@ -76,12 +116,12 @@ class add_screen extends Component {
 
                 <h4 className="mt-0 header-title">Location</h4>
 
-                <p className="text-muted m-b-30 font-14">Mobile App Notifications allow you to keep in touch with your users in a non-intrusive way by providing timely messages and helpful and relevant information like rewards descriptions and special offers. They also provide a great way of getting users to engage in new promotions or features...</p>
+                <p className="text-muted m-b-30 font-14">Mobile App Notifications allow you to keep in touch with your users in a non-intrusive way by providing timely messages and helpful and relevant information like rewards descriptions and special offers. They also provide a great way of getting users to engage in new promotions or features...<br/>* are Mandatory Fields</p>
                 {/* <SearchLocationInput/> */}
                 <div className="row">
                   <div className="col-6">
                     <div className="form-group">
-                      <label className="control-label">Select City</label>
+                      <label className="control-label">Select City*</label>
                       <select className="form-control select2" ref={(c) => this.screenCity = c}>
                         <option>Select</option>
                         <optgroup label="Available Locations">
@@ -91,7 +131,7 @@ class add_screen extends Component {
                       </select></div>
                   </div>
                   <div className="col-6">
-                    <label>Screen Status</label><br></br>
+                    <label>Screen Status*</label><br></br>
                     <input type="checkbox" id="screenStatus" switch="bool" checked={this.state.switch ? "checked" : ""} onChange={this.changeswitch} />
                     <label htmlFor="screenStatus" data-on-label="On" data-off-label="Off"></label>
                   </div>
@@ -99,14 +139,14 @@ class add_screen extends Component {
                 <div className="row">
                   <div className="col-6">
                     <div className="form-group">
-                      <label className="col-form-label">Location</label>
+                      <label className="col-form-label">Location*</label>
                       <input className="form-control" defaultValue="" placeholder="Screen Location" type="text" name='location' ref={(c) => this.screenLocation = c} />
                     </div>
 
                   </div>
                   <div className="col-6">
                     <div className="form-group">
-                      <label className="col-form-label">Pin Code</label>
+                      <label className="col-form-label">Pin Code*</label>
                       <input className="form-control" defaultValue="" type="text" placeholder="PinCode" name="screenPincode" ref={(c) => this.screenPincode = c} />
                     </div>
 
@@ -115,13 +155,13 @@ class add_screen extends Component {
                 <div className="row">
                   <div className="col-6">
                     <div className="form-group">
-                      <label className="col-form-label">Impressions Per Day</label>
+                      <label className="col-form-label">Impressions Per Day*</label>
                       <input className="form-control" type="number" placeholder="Enter only digits" defaultValue="" name="impression" ref={(c) => this.impression = c} />
                     </div>
                   </div>
                   <div className="col-6">
                     <div className="form-group">
-                      <label className="col-form-label">Gender Ratio (M/F)</label>
+                      <label className="col-form-label">Gender Ratio (M/F)*</label>
                       <input className="form-control" placeholder="Enter Male over female ratio" type="number" defaultValue="" name="gender_ratio" ref={(c) => this.gender_ratio = c} />
                     </div>
                   </div>
@@ -135,13 +175,13 @@ class add_screen extends Component {
                         </div> */}
                   <div className="col-6">
                     <div className="form-group">
-                      <label className="col-form-label">Price</label>
+                      <label className="col-form-label">Price*</label>
                       <input className="form-control" type="number" placeholder="Amount per day" defaultValue="" name="price" ref={(c) => this.price = c} />
                     </div>
                   </div>
 			<div className="col-6">
                             <div className="form-group">
-                            <label className="col-form-label">Screen Active Time (Format ex: 9AM - 5PM)</label>
+                            <label className="col-form-label">Screen Active Time (Format ex: 9AM - 5PM)*</label>
                       <input className="form-control" placeholder="SAT in Above Format only." type="text" defaultValue="" name="screenActiveTime" ref={(c) => this.screenActiveTime = c} />
                             </div>
                         </div>
@@ -149,29 +189,29 @@ class add_screen extends Component {
                 <div className="row">
                   <div className="col-12">
                             <div class="form-group">
-                              <label>Age Group Pref:</label><br />
+                              <label>Age Group Pref:*</label><br />
                                 <div className="row">
 <div className="col-3">
                                     <div className="form-group">
-                                      <label className="col-form-label">Below 18 :{/* (Baby Boomers)*/}</label>
+                                      <label className="col-form-label">Below 18 :*{/* (Baby Boomers)*/}</label>
                                       <input className="form-control" type="number" name="babyBoomers" onChange={this.handleAgeGroupChange} />
                                     </div>
                                 </div>
  <div className="col-3">
                                     <div className="form-group">
-                                      <label className="col-form-label">Age 18-34:{/* (Generation X)*/}</label>
+                                      <label className="col-form-label">Age 18-34:*{/* (Generation X)*/}</label>
                                       <input className="form-control" type="number" name="generationX" onChange={this.handleAgeGroupChange} />
                                     </div>
                                 </div>
  <div className="col-3">
                                     <div className="form-group">
-                                      <label className="col-form-label">Age 35-50:{/* (Generation Y)*/}</label>
+                                      <label className="col-form-label">Age 35-50:*{/* (Generation Y)*/}</label>
                                       <input className="form-control" type="number" name="generationY" onChange={this.handleAgeGroupChange}  />
                                     </div>
                                 </div>
                                   <div className="col-3">
                                     <div className="form-group">
-                                      <label className="col-form-label">Age 50+:{/* (Generation Z)*/}</label>
+                                      <label className="col-form-label">Age 50+:*{/* (Generation Z)*/}</label>
                                       <input className="form-control" type="number" name="generationZ" onChange={this.handleAgeGroupChange}  />
                                     </div>
                                 </div>
